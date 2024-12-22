a109b0a643d6023467724c59bbe2c68f1820f5d6
Gaurav07004 <singhgaurav07004@gmail.com>
2024-12-22 12:27:24 +0530

    Database Update and ApI

--- src/pages/api/auth/dashboard.ts
+++ src/pages/api/auth/dashboard.ts
@@ -3,27 +3,26 @@ import authenticateMiddleware from '@/middleware/authMiddleware';
 import Customer from '@/models/Customer';
 import Order from '@/models/Order';
 import Product from '@/models/Product';
-import UnifiedCollection from '@/models/Unified';
 
 const dashboardHandler = async (req: NextApiRequest, res: NextApiResponse) => {
     try {
-        // const customers = await Customer.find();
-        // const orders = await Order.find();
-        // const products = await Product.find();
-        const dashboards = await UnifiedCollection.find().limit(10).skip(20);
+        const customers = await Customer.find();
+        const orders = await Order.find();
+        const products = await Product.find();
 
-        // console.log('Fetched data:', { customers, orders, products, dashboards });
         res.status(200).json({
             message: 'Authenticated successfully',
             admin: req.admin,
-            // customers,
-            // orders,
-            // products,
-            dashboards,
+            customers,
+            orders,
+            products,
         });
     } catch (error) {
         console.error('Error fetching data:', error);
-        res.status(500).json({ message: 'Error fetching data', error: error.message });
+
+        const errorMessage = error instanceof Error ? error.message : 'An unknown error occurred';
+
+        res.status(500).json({ message: 'Error fetching data', error: errorMessage });
     }
 };
 
