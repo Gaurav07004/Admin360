af667e51ee09c2707cfc00292ef989e0c7b52eca
Gaurav07004 <singhgaurav07004@gmail.com>
2025-01-02 11:17:04 +0530

    Resolved General Error

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,19 +1,38 @@
 import { configureStore } from '@reduxjs/toolkit';
-import menuReducer from './slices/commonSlice';
-import userReducer from './slices/adminSlice';
-import productsReducer from './slices/productsSlice';
-import customerReducer from './slices/customerSlice';
-import orderReducer from './slices/orderSlice';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+
+import menuReducer from '@/redux/slices/commonSlice';
+import userReducer from '@/redux/slices/adminSlice';
+import productsReducer from '@/redux/slices/productsSlice';
+import customerReducer from '@/redux/slices/customerSlice';
+import orderReducer from '@/redux/slices/orderSlice';
+
+const rootReducer = combineReducers({
+    menu: menuReducer,
+    user: userReducer,
+    product: productsReducer,
+    customer: customerReducer,
+    order: orderReducer,
+});
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-    reducer: {
-        menu: menuReducer,
-        user: userReducer,
-        product: productsReducer,
-        customer: customerReducer,
-        order: orderReducer
-    },
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
 });
+
+export const persistor = persistStore(store);
 
 export default store;
 
