0a3186cccabb245302cd088c2a7e21612c77d1c5
Gaurav07004 <singhgaurav07004@gmail.com>
2025-01-03 12:21:50 +0530

    Resolved General Error

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -71,7 +71,7 @@ const loginAdmin = async (req: NextApiRequest, res: NextApiResponse) => {
             isActive: admin.isActive,
         };
 
-        const token = jwt.sign(payload, process.env.JWT_SECRET as string, { expiresIn: '1hr' });
+        const token = jwt.sign(payload, process.env.JWT_SECRET as string, { expiresIn: '1h' });
 
         return res.status(200).json({ message: "Login successful", token });
     } catch (error) {
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { combineReducers } from 'redux';
 
-import menuReducer from '@/redux/slices/commonSlice';
-import userReducer from '@/redux/slices/adminSlice';
-import productsReducer from '@/redux/slices/productsSlice';
-import customerReducer from '@/redux/slices/customerSlice';
-import orderReducer from '@/redux/slices/orderSlice';
+import menuReducer from './slices/commonSlice';
+import userReducer from './slices/adminSlice';
+import productsReducer from './slices/productsSlice';
+import customerReducer from './slices/customerSlice';
+import orderReducer from './slices/orderSlice';
 
 const rootReducer = combineReducers({
     menu: menuReducer,
@@ -15,13 +17,22 @@ const rootReducer = combineReducers({
     order: orderReducer,
 });
 
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({
             serializableCheck: false,
         }),
 });
+
+export const persistor = persistStore(store);
 
 export default store;
 
